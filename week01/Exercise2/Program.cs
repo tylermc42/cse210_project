f4f7fdd43b60d0ecf5c4843f3cd6afab8e6fdbe9
tylermc42 <freighttrain42@gmail.com>
2025-03-06 02:46:01 -0700

    Exercise 2 Week 1

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,35 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        // Get number grade and convert to an integer
+        Console.Write("What is your grade? ");
+        string number = Console.ReadLine();
+        int grade = int.Parse(number);
+
+        string letter = "";
+
+        // write if statments to print correct response for letter grade
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+        else if (grade >= 80 && grade < 90)
+        {
+            letter = "B";
+        }
+        else if (grade >= 70 && grade < 80)
+        {
+            letter = "C";
+        }
+        else if (grade >= 60 && grade < 70)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"You recieved a {letter}");
     }
 }
