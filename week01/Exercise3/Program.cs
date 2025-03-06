b90dac46f7855bfbbee5cecec586dc0094337e6e
tylermc42 <freighttrain42@gmail.com>
2025-03-06 03:26:07 -0700

    Week 1 Exercise 3-loops

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -1,9 +1,38 @@
 using System;
+using System.Formats.Asn1;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        //set magic number
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1,101);
+
+        int userNumber = -1;
+
+        // use loops to give hints till guess is right
+        while (userNumber != magicNumber)
+        {
+            // get number from user
+            Console.Write("What is the magic number? ");
+            userNumber = int.Parse(Console.ReadLine());
+
+            if (userNumber > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (userNumber < magicNumber)
+            {
+                Console.WriteLine("Higher");
+                
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        }
+        
     }
 }
+
