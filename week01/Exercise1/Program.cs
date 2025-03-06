acef91c821bf50119131d871e96edf4936e336b5
tylermc42 <freighttrain42@gmail.com>
2025-03-06 02:08:29 -0700

    added a comment

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,9 +4,10 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.Write("What is your first name?");
+        // Ask the user for their name.
+        Console.Write("What is your first name? ");
         string fname = Console.ReadLine();
-        Console.Write("What is your last name?");
+        Console.Write("What is your last name? ");
         string lname = Console.ReadLine();
 
         Console.WriteLine($"Your name is {lname}, {fname} {lname}.");
