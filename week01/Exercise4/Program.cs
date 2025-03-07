9e8c88b5fe87b184e084df81cfbf31b1fddb26ed
tylermc42 <freighttrain42@gmail.com>
2025-03-07 01:09:43 -0700

    Week 1 Exercise 4

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -4,6 +4,49 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        //create list
+        List<int> numbers = new List<int>();
+
+        // set variable
+        int userNumber = -1;
+        int sum = 0;
+        int largest = 0;
+
+        //start a loop to let user add numbers to a list
+        while (userNumber != 0)
+        {
+            //get number
+            Console.Write("Enter number(type 0 to quit): ");
+            userNumber = int.Parse(Console.ReadLine());
+            //add number to list
+            if (userNumber != 0)
+            {
+                numbers.Add(userNumber);
+            }
+            
+        }
+
+        //find the sum of the list
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+        Console.WriteLine($"The sum is: {sum}");
+
+        //find average of list
+        float average = ((float)sum) / numbers.Count;
+        Console.WriteLine($"The average is : {average}");
+
+
+        //find highest nubmer
+        foreach (int number in numbers)
+        {
+            if (number > largest)
+            {
+                largest = number;
+            }
+        }
+        Console.WriteLine($"The largest number is: {largest}");
+        
     }
 }
